ecf2cd263e3bc68529328b13822cba65700d9596
Werimpossible <andrea.gurod@gmail.com>
2017-10-24 09:13:13 -0500

    Echandole ganas a subir mi primer README.md

---
+++ README.md
@@ -0,0 +1,6 @@
+###PSEUCODIGO
+
+1.Crear una función que reciba dos arreglos
+2.Comprobar si los dos arreglos tienen la misma longitud
+  2.1 Si son iguales, el programa debe regresar false
+  2.2.
---
+++ app.js
@@ -0,0 +1,13 @@
+var comparar = function (arreglo1, arreglo2){
+  If(arreglo1.lenght !== arreglo2.lenght){
+    return false;
+  }else{
+    for var i=0; i<arreglo1.lenght; i+=1{
+      
+    }
+  }
+
+}
+
+var array1=[1,2,3,4,5,6,7,8,9];
+var array2=[1,2,3,4,5,6,7,8,9];
